36a826c94bee66422c596db4b2d29ed73560f2c5
RehanRiaz20 <rehanriaz991@gmail.com>
2024-01-22 15:21:27 +0500

    sidebar updated
    modified sidebar to make it collapsible.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,58 @@
+// // Sidebar.jsx
+// //product - order - dispatch - delivered - returned 
+// import React from 'react';
+// import './css/Sidebar.css';
+
+// const Sidebar = () => {
+//   return (
+//     <div className="sidebar">
+//       <h2>Sidebar</h2>
+//       <ul>
+//         <li>Product</li>
+//         <li>Order</li>
+//         <li>Dispatch</li>
+//         <li>Delivered</li>
+//         <li>Returned</li>
+//         {/* Add more items as needed */}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
 // Sidebar.jsx
 
-import React from 'react';
-import './css/Sidebar.css';
+import React, { useState } from 'react';
+import './css//Sidebar.css';
 
 const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleToggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="sidebar">
-      <h2>Sidebar</h2>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        {/* Add more items as needed */}
-      </ul>
+    <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
+      <div className="toggle-button" onClick={handleToggleCollapse}>
+        {collapsed ? <h2>»</h2> : <h1>«</h1>}
+      </div>
+      {!collapsed && (
+        <>
+          {/* <h2>Sidebar</h2> */}
+          <ul>
+          <li>Product</li>
+          <li>Order</li>
+          <li>Dispatch</li>
+          <li>Delivered</li>
+          <li>Returned</li>
+            {/* Add more items as needed */}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
 
 export default Sidebar;
-
